7671538a70e9c7052ec4a8f2988384e7c496789e
henriquejosefaria <henriquejosefaria@gmail.com>
2019-12-04 11:12:33 +0000

    update

--- mongo.py
+++ mongo.py
@@ -23,8 +23,8 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 #leitura DB
-firstQuery = "SELECT f.title,f.release_year,f.description,c.name AS Category,a.first_name,a.last_name, language.name AS Foreign_language,extra.name AS Original_language FROM film AS f INNER JOIN film_category AS fc ON fc.film_id = f.film_id INNER JOIN category AS c ON c.category_id = fc.category_id INNER JOIN film_actor AS fa ON fa.film_id = f.film_id INNER JOIN actor AS a ON a.actor_id = fa.actor_id INNER JOIN language ON language.language_id = f.language_id LEFT JOIN ( SELECT f.film_id,l.name FROM film AS f INNER JOIN language AS l ON l.language_id = f.original_language_id WHERE f.original_language_id is not null) extra ON f.film_id = extra.film_id"
-secondQuery = "SELECT s.store_id, c.first_name AS Costumer_first_name, c.last_name AS Costumer_last_name, p.amount, p.payment_date, st.first_name AS Staff_first_name, st.last_name  AS Staff_last_name FROM store AS s INNER JOIN customer AS c ON s.store_id = s.store_id INNER JOIN payment AS p ON c.customer_id = p.customer_id INNER JOIN staff AS st ON p.staff_id = st.staff_id"
+firstQuery = "SELECT f.title,f.release_year,f.description,c.name AS Category,a.first_name,a.last_name, language.name AS Foreign_language,extra.name AS Original_language,f.film_id FROM film AS f LEFT JOIN film_category AS fc ON fc.film_id = f.film_id LEFT JOIN category AS c ON c.category_id = fc.category_id LEFT JOIN film_actor AS fa ON fa.film_id = f.film_id LEFT JOIN actor AS a ON a.actor_id = fa.actor_id LEFT JOIN language ON language.language_id = f.language_id LEFT JOIN ( SELECT f.film_id,l.name FROM film AS f LEFT JOIN language AS l ON l.language_id = f.original_language_id WHERE f.original_language_id is not null) extra ON f.film_id = extra.film_id"
+secondQuery = "SELECT s.store_id, c.first_name AS Costumer_first_name, c.last_name AS Costumer_last_name, p.amount, p.payment_date, st.first_name AS Staff_first_name, st.last_name  AS Staff_last_name FROM store AS s LEFT JOIN customer AS c ON s.store_id = s.store_id LEFT JOIN payment AS p ON c.customer_id = p.customer_id LEFT JOIN staff AS st ON p.staff_id = st.staff_id"
 mycursor.execute(firstQuery)
 filmRecords = mycursor.fetchall()
 mycursor.execute(secondQuery)
@@ -32,30 +32,36 @@ storeRecords = mycursor.fetchall()
 print(len(filmRecords))
 print(len(storeRecords))
 myFilmIt = iter(filmRecords)
-titleaux = ""
+i1aux = None
 categorys = []
 actors = []
+filmsList.drop()
+paymentList.drop()
 while(True):
     try:
         i1 = next(myFilmIt)
     except StopIteration:
         print("first collection done")
         break
-    title = i1[0]
-    if titleaux == "":
-        titleaux = title
-    year  = i1[1]
-    desc = i1[2]
-    language = i1[6]
-    orig_language = i1[7]
+    if i1aux == None:
+        i1aux = i1
     # category and actors
-    if titleaux == title:
-        if i1[3] not in categorys :
-            categorys.append(i1[3])
+    if i1aux[0] == i1[0]:
         actors.append(i1[4] + " " + i1[5])
+        if i1[3] not in categorys:
+            categorys.append(i1[3])
     else:
-        info = { "title" : title, "release_year" : year, "descrição" : desc, "original_language" : orig_language, "foreign_language" : language, "categorys" :  categorys, "actors" : actors}
+        print(i1[0])
+        info = { "id": i1aux[8], "title" : i1aux[0], "release_year" : i1aux[1], "descrição" : i1aux[2], "original_language" : i1aux[7], "foreign_language" : i1aux[6], "categorys" :  categorys, "actors" : actors}
         filmsList.insert_one(info)
+        actors.clear()
+        categorys.clear()
+    i1aux = i1
+    print("titleaux:")
+    print(i1aux[0])
+
+info = {"id": i1aux[8], "title": i1aux[0], "release_year": i1aux[1], "descrição": i1aux[2], "original_language": i1aux[7],"foreign_language": i1aux[6], "categorys": categorys, "actors": actors}
+filmsList.insert_one(info)
 myStoreIt = iter(storeRecords)
 while(True):
     try:
@@ -70,6 +76,10 @@ while(True):
     store = i2[0]
     storeinfo = {"store_id" : store, "customer" : costumer, "date" : date, "amount" : amount, "staff" : staff}
     paymentList.insert_one(storeinfo)
+while(True):
+    query1Mongo = filmsList.find({}, {"title": 1, "original_language": 1, "foreign_language": 1, "_id": 0})
+    [print(queryMongo) for queryMongo in query1Mongo]
+
 
 
 
